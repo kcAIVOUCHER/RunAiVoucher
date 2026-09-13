766d658cc941b5be6963d41a041c0b807f85ea2e
kcAIVOUCHER <kcarrascosa.comercial@gmail.com>
2026-09-13 16:05:15 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import {AuthProvider} from './contexts/AuthContext';
+import {ErrorBoundary} from './components/ErrorBoundary';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ErrorBoundary fallbackTitle="Falha na inicialização da aplicação">
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ErrorBoundary>
   </StrictMode>,
 );
